2a5b20c4cc2a39c68fae707687cd69193a0fd3f7
prafulpathare <patharepraful@gmail.com>
2024-08-26 00:48:40 +0530

    masthead and header

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="header">
+
+        <nav>
+          {menu.map((item: IdLabel) => (
+            <a href="#">
+              {item.label}
+            </a>
+          ))}
+        </nav>
+      </div>
+
+      <div className="mast-head">
+        <img src="https://images-porsche.imgix.net/-/media/118CC27923D348479FFFA724C52F05F8_DDC0C8C251894632B424B4D2D985AB7E_TA24Q3DIX0010-taycan-turbo-side?w=2560&h=697&q=45&crop=faces%2Centropy%2Cedges&auto=format" id="mto-img" />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+const menu: IdLabel[] = [
+  {
+    id: 2828934,
+    label: 'About',
+  },
+  {
+    id: 2881934,
+    label: 'My Account',
+  },
+  {
+    id: 288934,
+    label: 'Home',
+  },
+]
+
+
+interface IdLabel {
+  id: number;
+  label: string;
+}
